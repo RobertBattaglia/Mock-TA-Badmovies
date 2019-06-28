ec7a609b65d10782b48de52a3befac32b5c407f9
Robert Battaglia <r083rt164@gmail.com>
2019-06-28 10:03:21 -0400

    query movies from api

--- server/helpers/apiHelpers.js
+++ server/helpers/apiHelpers.js
@@ -2,11 +2,14 @@ const request = require('request');
 const axios = require('axios');
 const { API_KEY } = require('../../config.js');
 
-// write out logic/functions required to query TheMovieDB.org
+const getMovies = () => {
+  const config = {
+    method: 'GET',
+    url: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.asc&include_adult=false&include_video=false`
+  };
+  return axios(config);
+};
 
-// FOR REFERENCE:
-// https://www.themoviedb.org/account/signup
-// https://developers.themoviedb.org/3/discover/movie-discover
-// Get your API Key and save it in your config file
-
-// Don't forget to export your functions and require them within your server file
+module.exports = {
+  getMovies
+};
