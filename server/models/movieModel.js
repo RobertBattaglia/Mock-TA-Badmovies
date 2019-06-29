d36fba6f02126d0fb21e1d7da360c05145ccea23
Robert Battaglia <r083rt164@gmail.com>
2019-06-29 13:53:10 -0400

    clean up comments and file system to only include mongoDB

--- server/models/movieModel.js
+++ server/models/movieModel.js
@@ -1,4 +1,3 @@
-//SELECT one db to work with
 //For Mongo
 const favorites = require('../../db/mongodb').favorites;
 
@@ -34,48 +33,3 @@ module.exports = {
       });
   }
 };
-
-//For SQL
-// const sqlDb = require('../../db/sql');
-
-// module.exports = {
-//   save: (movie, cb) => {
-//     const sql = `INSERT INTO favorites
-//     (id, title, poster_path, release_date, popularity)
-//     VALUES (?, ?, ?, ?, ?)`;
-//     const vals = [
-//       movie.id,
-//       movie.title,
-//       movie.poster_path,
-//       movie.release_date,
-//       movie.popularity
-//     ];
-//     sqlDb.query(sql, vals, (err, data) => {
-//       if (err) {
-//         cb(err);
-//       } else {
-//         cb(null, data);
-//       }
-//     });
-//   },
-//   delete: (id, cb) => {
-//     const sql = `DELETE FROM favorites WHERE id = (?)`;
-//     sqlDb.query(sql, [id], (err, data) => {
-//       if (err) {
-//         cb(err);
-//       } else {
-//         cb(null, data);
-//       }
-//     });
-//   },
-//   get: cb => {
-//     const sql = 'SELECT * FROM favorites';
-//     sqlDb.query(sql, null, (err, data) => {
-//       if (err) {
-//         cb(err);
-//       } else {
-//         cb(null, data);
-//       }
-//     });
-//   }
-// };
